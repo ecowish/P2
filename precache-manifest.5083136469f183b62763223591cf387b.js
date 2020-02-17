f4d21173dfc54fdac5a3de8642472eaee97e6e83
Lee Quirax <quirax@QrMacBook.local>
2020-02-17 16:10:57 +0900

    Updates

---
+++ precache-manifest.5083136469f183b62763223591cf387b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250dfcb90d22d45fa0c804a4887a4beb",
+    "url": "quirax.github.io/P2/index.html"
+  },
+  {
+    "revision": "9e9c14f4e978a454a2ac",
+    "url": "quirax.github.io/P2/static/css/main.f0876414.chunk.css"
+  },
+  {
+    "revision": "57dddce44f4e888b97b3",
+    "url": "quirax.github.io/P2/static/js/2.416ff4f0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "quirax.github.io/P2/static/js/2.416ff4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e9c14f4e978a454a2ac",
+    "url": "quirax.github.io/P2/static/js/main.9a52b1bd.chunk.js"
+  },
+  {
+    "revision": "6a09f1d3af2c6fdcf375",
+    "url": "quirax.github.io/P2/static/js/runtime-main.4cbd3290.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "quirax.github.io/P2/static/media/logo.5d5d9eef.svg"
+  }
+]);
